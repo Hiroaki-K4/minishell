eb38d1fc56b6e308a46864073dd63b01ce623480
Hiroaki-K4 <49no19@gmail.com>
2021-10-20 16:27:56 +0900

    make run_command function

--- srcs/main.c
+++ srcs/main.c
@@ -25,7 +25,10 @@ void	run_command(char *command, char *envp[])
 	{
 		path = ft_strjoin("/bin/", command);
 		if (execve(path, argv, envp) == -1)
+		{
 			printf("Error\n");
+			exit(1);
+		}
 	}
 }
 
