5e2e244d293094046c3a6137e82f595cb2d89c44
Hiroaki-K4 <49no19@gmail.com>
2021-10-26 21:39:16 +0900

    make tokenize function

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -7,15 +7,8 @@ char	*delete_space(char *command)
 	char	*trimed2;
 
 	trimed1 = ft_strtrim(command, " ");
-	trimed2 = ft_strtrim(trimed1, "\f");
+	trimed2 = ft_strtrim(trimed1, "\t");
 	free(trimed1);
-	trimed1 = ft_strtrim(trimed2, "\n");
-	free(trimed2);
-	trimed2 = ft_strtrim(trimed1, "\r");
-	free(trimed1);
-	trimed1 = ft_strtrim(trimed2, "\t");
-	free(trimed2);
-	trimed2 = ft_strtrim(trimed1, "\v");
 	return (trimed2);
 }
 
@@ -29,13 +22,16 @@ void	init_command_list(t_command *command_list)
 void	tokenize(char *trimed, t_command *command_list)
 {
 	int	i;
+	int	pos;
 
 	(void)command_list;
 	printf("trimed: %s\n", trimed);
 	i = 0;
+	pos = 0;
 	while (trimed[i])
 	{
-		printf("%s\n", trimed[i]);
+		if
+		printf("%c\n", trimed[i]);
 		i++;
 	}
 }
