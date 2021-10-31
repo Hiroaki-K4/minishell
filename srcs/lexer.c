202763fcab738941f5064aa6771153c40ef53f8d
Hiroaki-K4 <49no19@gmail.com>
2021-10-31 17:13:49 +0900

    make store_token function

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -38,7 +38,7 @@ int	store_token(char *trimed, t_command **command_list, int pos, int i, int deli
 			return (-1);
 		last = get_last_list(*command_list);
 		printf("pos: %d i-pos: %d\n", pos, i - pos);
-		new->context = ft_substr(trimed, pos, i - pos);
+		new->context = ft_substr(trimed, pos, i + 1 - pos);
 		printf("new: %s\n", (char *)new->context);
 		new->next = NULL;
 		last->next = new;
