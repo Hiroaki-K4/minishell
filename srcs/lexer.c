071f24c4bf3fef641447909fe777cf28219cebda
Hiroaki-K4 <49no19@gmail.com>
2021-11-13 17:01:08 +0900

    clean store_token function

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -81,10 +81,8 @@ int	store_token(char *trimed, t_command *last, int pos, int *i)
 			split->attr = DQUOTE;
 	}
 	split->next = NULL;
-	printf("i: %d pos: %d trim: %c\n", *i, pos, trimed[*i]);
 	if (*i - pos == 0)
 	{
-		printf("i - pos: %d str: %c\n", *i - pos, trimed[*i]);
 		free(new);
 		last->next = split;
 	}
