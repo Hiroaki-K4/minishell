7154daab99fd6af3eb3c65ffde2d9c54271682c8
Hiroaki-K4 <49no19@gmail.com>
2021-11-15 23:06:28 +0900

    Delete waste STR attribute

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -57,6 +57,7 @@ int	store_token(char *trimed, t_command *last, int pos, int *i)
 	else
 	{
 		new->context = ft_substr(trimed, pos, *i - pos);
+		printf("*i - pos: %d\n", *i - pos);
 		new->attr = STR;
 		if ((trimed[*i] == '<' && trimed[*i + 1] == '<') || (trimed[*i] == '>' && trimed[*i + 1] == '>'))
 		{
