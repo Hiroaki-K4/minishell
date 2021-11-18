486c518e44499131ed58ee2a57f20d49d231e6d5
Hiroaki-K4 <49no19@gmail.com>
2021-11-18 22:08:52 +0900

    Change lexer function

--- srcs/operate_list.c
+++ srcs/operate_list.c
@@ -1,10 +1,10 @@
 #include "minishell.h"
 
-void	init_command_list(t_command *command_list)
+void	init_command_list(t_command **command_list)
 {
-	command_list->context = "";
-	command_list->attr = START;
-	command_list->next = NULL;
+	(*command_list)->context = "";
+	(*command_list)->attr = START;
+	(*command_list)->next = NULL;
 }
 
 t_command	*get_last_list(t_command *list)
