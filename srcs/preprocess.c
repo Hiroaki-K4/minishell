ececfc8e51dc3acbd41d07713b865d872f0a80ad
Hiroaki-K4 <49no19@gmail.com>
2021-11-23 15:21:51 +0900

    #33 Make check_syntax function

--- srcs/preprocess.c
+++ srcs/preprocess.c
@@ -35,6 +35,31 @@ void	tokenize(char *trimed, t_list **command_list)
 	}
 }
 
+int		check_syntax(t_list *command_list)
+{
+	int	first_flag;
+	t_command	*content;
+	t_command	*next_content;
+	
+	first_flag = 1;
+	command_list = command_list->next;
+	while (command_list->next != NULL)
+	{
+		content = (t_command *)command_list->content;
+		next_content = (t_command *)command_list->next->content;
+		if (first_flag == 1 && content->attr == PIPE)
+			return (-1);
+		else if (content->attr == PIPE && next_content->attr == PIPE)
+			printf("double pipe\n");
+			// return (-1);
+		if (first_flag == 1)
+			first_flag = 0;
+		command_list = command_list->next;
+		printf("ok\n");
+	}
+	return (0);
+}
+
 void	parser(t_list **command_list)
 {
 	(void)command_list;
@@ -60,6 +85,8 @@ int	preprocess(char *line)
 	if (!command_list)
 		return (-1);
 	tokenize(trimed, &command_list);
+	if (check_syntax(command_list) == -1)
+		return (-1);
 	parser(&command_list);
 	ft_lstiter(command_list, output_result);
 	return (0);
