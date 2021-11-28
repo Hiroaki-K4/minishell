4bbed6c1426370ecff3c8c1b1df7c59f23b6f734
Hiroaki-K4 <49no19@gmail.com>
2021-11-28 17:03:06 +0900

    #43 Add check_result function in test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+check_result() {
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+}
+
+# Test make command
 make
+check_result
 make clean
+check_result
 make re
+check_result
+make fclean
+check_result
+
+# Test Tokenizer
+make
+echo "aaa" | ./minishell
 make fclean
