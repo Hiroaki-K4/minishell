f6a7dfc9dab33878686fa3617a86c4e2f18960d0
Hiroaki Kubo <kubo@globalwalkers.co.jp>
2022-01-04 21:26:23 +0900

    #60 Implemented a rough process of expansion

--- srcs/expand.c
+++ srcs/expand.c
@@ -1,20 +1,47 @@
 #include "minishell.h"
 
+int	get_next_dollar_pos(char *content, int pos)
+{
+	pos++;
+	while (content[pos] && content[pos] != '$')
+		pos++;
+	return (pos);
+}
+
+int	get_env_first_pos(char *content, char **env, int start, int next_dollar)
+{
+	int	env_pos;
+
+	env_pos = start;
+	while (env_pos < next_dollar && content[env_pos] != ' ')
+		env_pos++;
+	*env = getenv(ft_substr(content, start, env_pos - start));
+	return (env_pos);
+}
+
 char	*expand_env_vals(char *content)
 {
-	int	i;
-	int	start;
+	int		i;
+	int		start;
+	int		env_pos;
+	char	*env;
 	char	*expanded;
 
 	i = 0;
 	start = 0;
+	expanded = ft_strdup("");
 	while (content[i])
 	{
-		if (content[i] == '$' && start != i)
+		if (content[i] == '$')
 		{
-			expanded = ft_strjoin(expanded, ft_substr(content, start, i - start));
-			printf("expanded: %s\n", expanded);
-			start = i;
+			if (start != i)
+				expanded = ft_strjoin(expanded, ft_substr(content, start, i - start));
+			env_pos = get_env_first_pos(content, &env, i + 1, get_next_dollar_pos(content, i));
+			if (env != NULL)
+				expanded = ft_strjoin(expanded, env);
+			i = env_pos;
+			start = env_pos;
+			continue ;
 		}
 		i++;
 	}
@@ -25,23 +52,21 @@ int	expand(t_list *token_list, t_list **expanded_list)
 {
 	t_token	*expanded_token;
 
-	(void)expanded_list;
-	expanded_token = (t_token *)malloc(sizeof(t_token));
-	while (token_list->next != NULL)
+	while (token_list != NULL)
 	{
-		if (((t_token *)(token_list->content))->attr == TK_SINGLE_QUOTED)
+		expanded_token = (t_token *)malloc(sizeof(t_token));
+		expanded_token->attr = ((t_token *)token_list->content)->attr;
+		if (expanded_token->attr == TK_SINGLE_QUOTED)
 			expanded_token->content = ft_strtrim(((t_token *)token_list->content)->content, "\'");
-		else if (((t_token *)(token_list->content))->attr == TK_DOUBLE_QUOTED)
+		else if (expanded_token->attr == TK_DOUBLE_QUOTED)
 			expanded_token->content = ft_strtrim(((t_token *)token_list->content)->content, "\"");
 		else
 			expanded_token->content = ((t_token *)token_list->content)->content;
-		expanded_token->content = expand_env_vals(expanded_token->content);
-		expanded_token->attr = ((t_token *)token_list->content)->attr;
+		if ((expanded_token->attr == TK_WORD || expanded_token->attr == TK_DOUBLE_QUOTED) && ft_strchr(expanded_token->content, '$') != NULL)
+			expanded_token->content = expand_env_vals(expanded_token->content);
 		if (ft_lstadd_node(expanded_list, expanded_token) == FAIL)
 			return (FAIL);
 		token_list = token_list->next;
 	}
-	if (token_list != NULL)
-		printf("final\n");
 	return (SUCCESS);
 }
