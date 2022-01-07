d67ae3de66d47ef3e0e75b7f1544cce7b287ff05
Hiroaki Kubo <kubo@globalwalkers.co.jp>
2022-01-07 22:46:46 +0900

    #62 Add set environment process

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -2,6 +2,11 @@
 
 int	ft_export(char **argv)
 {
+	size_t	i;
+	size_t	j;
+	char	*val;
+	char	*word;
+
 	if (argv[1] == NULL)
 	{
 		extern char	**environ;
@@ -20,7 +25,44 @@ int	ft_export(char **argv)
 	}
 	else
 	{
-		printf("two\n");
+		i = 1;
+		while (argv[i])
+		{
+			printf("i: %ld\n", i);
+			j = 0;
+			if (ft_strchr(argv[i], '=') == NULL)
+			{
+				val = argv[i];
+				word = NULL;
+				printf("val: %s word: %s\n", val, word);
+				if (setenv(val, word, 1) == -1)
+					printf("FAILED\n");
+			}
+			else
+			{
+				while (argv[i][j])
+				{
+					if (argv[i][j] == '=')
+					{
+						if (j == 0)
+							return (FAIL);
+						else
+						{
+							val = ft_substr(argv[i], 0, j);
+							word = ft_substr(argv[i], j + 1, ft_strlen(argv[i]) - j);
+							if (word == NULL)
+								word = ft_strdup("");
+							printf("val: %s word: %s\n", val, word);
+							if (setenv(val, word, 1) == -1)
+								printf("FAILED\n");
+						}
+					}
+					j++;
+				}
+			}
+
+			i++;
+		}
 	}
 	return (SUCCESS);
 }
