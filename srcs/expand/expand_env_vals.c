fe0f72e15caad9f74e961bce28489b08016c12ad
Hiroaki Kubo <kubo@globalwalkers.co.jp>
2022-01-18 21:51:09 +0900

    #70 Change function name in expand_env_vals.c

--- srcs/expand/expand_env_vals.c
+++ srcs/expand/expand_env_vals.c
@@ -1,7 +1,6 @@
 #include "minishell.h"
 
-char	*get_env_from_extracted_word(t_expand_state *expand_state,
-			char *env, size_t start, t_envs *envs)
+char	*get_name_after_dollar(t_expand_state *expand_state, size_t start)
 {
 	while (expand_state->expanded_token->content[expand_state->current_pos]
 		&& expand_state->expanded_token
@@ -25,9 +24,8 @@ char	*get_env_from_extracted_word(t_expand_state *expand_state,
 	}
 	if (expand_state->current_pos == start)
 		return (ft_strdup("$"));
-	env = get_env(ft_substr(expand_state->expanded_token->content, start,
-				expand_state->current_pos - start), envs);
-	return (env);
+	return (ft_substr(expand_state->expanded_token->content,
+			start, expand_state->current_pos - start));
 }
 
 void	check_diff_between_start_and_curernt_pos(t_expand_state *expand_state,
@@ -42,7 +40,8 @@ void	check_diff_between_start_and_curernt_pos(t_expand_state *expand_state,
 
 char	*expand_env_vals(t_expand_state *expand_state, t_envs *envs)
 {
-	char	*env;
+	char	*name;
+	char	*value;
 	char	*expanded;
 
 	init_expand_state(expand_state);
@@ -54,10 +53,15 @@ char	*expand_env_vals(t_expand_state *expand_state, t_envs *envs)
 		{
 			check_diff_between_start_and_curernt_pos(expand_state, &expanded);
 			expand_state->current_pos++;
-			env = get_env_from_extracted_word(expand_state, env,
-					expand_state->current_pos, envs);
-			if (env != NULL)
-				expanded = ft_strjoin(expanded, env);
+			name = get_name_after_dollar(expand_state, expand_state->current_pos);
+			if (ft_strncmp(name, "$", ft_strlen(name) + 1) == 0)
+				expanded = ft_strjoin(expanded, name);
+			else
+			{
+				value = get_env(name, envs);
+				if (value != NULL)
+					expanded = ft_strjoin(expanded, value);
+			}
 			expand_state->start = expand_state->current_pos;
 			continue ;
 		}
