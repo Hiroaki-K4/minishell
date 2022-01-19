bb900c558acf4cfa42cc57d982933e7119472b41
Hiroaki-K4 <49no19@gmail.com>
2022-01-19 21:43:58 +0900

    #73 Add test case for expansion

--- test.sh
+++ test.sh
@@ -47,9 +47,6 @@ done
 rm -rf test/result/expand
 mkdir -p test/result/expand
 for fp in `ls test/case/expand`; do
-	if [ $fp = "check_second_tokenize.txt" ]; then
-		continue
-	fi
 	while read line
 	do
 		eval ${line} >> test/result/expand/$fp
