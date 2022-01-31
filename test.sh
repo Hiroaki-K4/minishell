5751bfd758efc67a27ff9f7151fdfe48e49e79b1
Hiroaki-K4 <49no19@gmail.com>
2022-01-31 22:27:46 +0900

    #84 Add test case about exit status

--- test.sh
+++ test.sh
@@ -63,9 +63,6 @@ done
 rm -rf test/result/expand/exit_status
 mkdir -p test/result/expand/exit_status
 for fp in `ls test/case/expand/exit_status`; do
-	if [ $fp = "fail_1_status.txt" ]; then
-		continue
-	fi
 	if [ $fp = "success_status_with_pipe.txt" ]; then
 		continue
 	fi
