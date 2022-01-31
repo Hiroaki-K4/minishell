e99f2dd0651b9360e3194d046fd770f2df43a44e
cheedah <cheedah@users.noreply.github.com>
2022-01-31 21:08:00 +0900

    fix(cd): add free ops

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -1,47 +1,54 @@
 #include "minishell.h"
 
-static void	convert_curpath_to_canonical_form(char **curpath)
+static void	concat_cdpath(char **argv, char **curpath, char *cdpath)
 {
-	(void)curpath;
-	return;
-}
-
-static void concat_cdpath(char **argv, char **curpath, char *cdpath)
-{
+	size_t	i;
 	char	*tmp;
+	char	*tmp2;
 	char	**paths;
 
+	i = 0;
 	paths = ft_split(cdpath, ':');
-	while (*paths)
+	if (paths == NULL || !*cdpath)
+		return ;
+	while (paths[i])
 	{
-		if (*paths[ft_strlen(*paths) - 1] == '/')
-		{
-			tmp = ft_strjoin(*paths, argv[1]);
-		}
+		if (paths[i][ft_strlen(*paths) - 1] == '/')
+			tmp = ft_strjoin(paths[i], argv[1]);
 		else
 		{
-			tmp = ft_strjoin(*paths, "/");
-			tmp = ft_strjoin(tmp, argv[1]);
+			tmp2 = ft_strjoin(paths[i], "/");
+			tmp = ft_strjoin(tmp2, argv[1]);
+			free(tmp2);
 		}
 		if (access(tmp, X_OK) == SUCCESS)
 		{
 			*curpath = tmp;
-			break;
+			break ;
 		}
-		paths++;
+		free(tmp);
+		free(paths[i]);
+		i++;
 	}
+	while (paths[i])
+	{
+		free(paths[i]);
+		i++;
+	}
+	free(paths);
 }
 
-static void set_curpath(char **argv, t_envs **envs, char **curpath)
+static void	set_curpath(char **argv, t_envs **envs, char **curpath)
 {
 	char	*tmp;
 	char	*cdpath;
 
 	if (*argv[1] == '/')
-		*curpath = argv[1];
+		*curpath = ft_strdup(argv[1]);
 	else
 	{
-		if (*argv[1] != '.' && ft_strncmp(argv[1], "..", ft_strlen(argv[1]) + 1))
+		if (*argv[1] != '.'
+			&& ft_strncmp(argv[1], "..", ft_strlen(argv[1]) + 1))
 		{
 			cdpath = get_env("CDPATH", *envs);
 			if (cdpath == NULL)
@@ -54,17 +61,19 @@ static void set_curpath(char **argv, t_envs **envs, char **curpath)
 				concat_cdpath(argv, curpath, cdpath);
 		}
 		if (*curpath == NULL)
-			*curpath = argv[1];
+			*curpath = ft_strdup(argv[1]);
 	}
 }
 
-static void concat_pwd(t_envs **envs, char **curpath)
+static void	concat_pwd(t_envs **envs, char **curpath)
 {
 	char	*pwd;
 	char	*tmp;
+	char	*tmp2;
 
 	pwd = NULL;
 	tmp = NULL;
+	tmp2 = NULL;
 	if (**curpath != '/')
 	{
 		pwd = get_env("PWD", *envs);
@@ -74,11 +83,19 @@ static void concat_pwd(t_envs **envs, char **curpath)
 		}
 		else
 		{
-			tmp = ft_strjoin(pwd, "/");
-			tmp = ft_strjoin(tmp, *curpath);
+			tmp2 = ft_strjoin(pwd, "/");
+			tmp = ft_strjoin(tmp2, *curpath);
+			free(tmp2);
 		}
+		free(*curpath);
 		*curpath = tmp;
 	}
+}
+
+static void	convert_curpath_to_canonical_form(char **curpath)
+{
+	(void)curpath;
+	return ;
 }
 
 int	ft_cd(char **argv, t_envs **envs, int *exit_status)
@@ -110,10 +127,12 @@ int	ft_cd(char **argv, t_envs **envs, int *exit_status)
 		ft_putstr_fd(": ", 2);
 		ft_putstr_fd(strerror(errno), 2);
 		ft_putstr_fd("\n", 2);
+		free(curpath);
 		*exit_status = 1;
 		return (1);
 	}
 	set_env("OLDPWD", get_env("PWD", *envs), envs);
 	set_env("PWD", curpath, envs);
+	free(curpath);
 	return (0);
 }
