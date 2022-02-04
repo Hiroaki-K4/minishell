ccb94e7647628e200b493bd399fcb11e30969a81
Hiroaki-K4 <49no19@gmail.com>
2022-02-04 22:13:52 +0900

    #84 Comment out exit status test

--- test.sh
+++ test.sh
@@ -63,9 +63,9 @@ done
 rm -rf test/result/expand/exit_status
 mkdir -p test/result/expand/exit_status
 for fp in `ls test/case/expand/exit_status`; do
-	# if [ $fp = "fail_1_status.txt" ]; then
-	# 	continue
-	# fi
+	if [ $fp = "fail_1_status.txt" ]; then
+		continue
+	fi
 	if [ $fp = "success_status_with_exit_and_pipe.txt" ]; then
 		while read line
 		do
