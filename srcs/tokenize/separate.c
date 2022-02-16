8255330509efa50a6a7fc2ac4f9db39ca007d514
Hiroaki-K4 <49no19@gmail.com>
2022-02-16 22:59:44 +0900

    #96 Change val name in tokenize

--- srcs/tokenize/separate.c
+++ srcs/tokenize/separate.c
@@ -1,75 +1,75 @@
 #include "minishell.h"
 
 int	separate_by_no_kind_sep_word(char *line, t_list **token_list,
-	t_tokenize_state *tokenize_state)
+	t_tokenize_state *t_state)
 {
 	size_t	i;
 	t_token	*new_token;
 
 	i = 0;
-	tokenize_state->token_kind = get_token_kind(line,
-			tokenize_state->current_pos, &i);
-	if (tokenize_state->trim_start != tokenize_state->current_pos)
+	t_state->token_kind = get_token_kind(line,
+			t_state->current_pos, &i);
+	if (t_state->trim_start != t_state->current_pos)
 	{
-		new_token = make_token(line, tokenize_state->trim_start,
-				tokenize_state->current_pos - tokenize_state->trim_start,
-				tokenize_state->token_kind);
+		new_token = make_token(line, t_state->trim_start,
+				t_state->current_pos - t_state->trim_start,
+				t_state->token_kind);
 		if (!new_token)
 			return (FAIL);
 		if (ft_lstadd_token(token_list, new_token) == FAIL)
 			return (FAIL);
 	}
-	tokenize_state->current_pos += i;
-	tokenize_state->trim_start = tokenize_state->current_pos;
+	t_state->current_pos += i;
+	t_state->trim_start = t_state->current_pos;
 	return (SUCCESS);
 }
 
 int	separate_by_sep_word(char *line, t_list **token_list,
-	t_tokenize_state *tokenize_state)
+	t_tokenize_state *t_state)
 {
 	size_t	i;
 	t_token	*new_token;
 
 	i = 0;
-	tokenize_state->token_kind = get_token_kind(line,
-			tokenize_state->current_pos, &i);
-	if (tokenize_state->trim_start != tokenize_state->current_pos)
+	t_state->token_kind = get_token_kind(line,
+			t_state->current_pos, &i);
+	if (t_state->trim_start != t_state->current_pos)
 	{
-		new_token = make_token(line, tokenize_state->trim_start,
-				tokenize_state->current_pos - tokenize_state->trim_start,
+		new_token = make_token(line, t_state->trim_start,
+				t_state->current_pos - t_state->trim_start,
 				TK_WORD);
 		if (!new_token)
 			return (FAIL);
 		if (ft_lstadd_token(token_list, new_token) == FAIL)
 			return (FAIL);
-		tokenize_state->trim_start = tokenize_state->current_pos;
+		t_state->trim_start = t_state->current_pos;
 	}
-	new_token = make_token(line, tokenize_state->trim_start,
-			tokenize_state->current_pos + i - tokenize_state->trim_start,
-			tokenize_state->token_kind);
+	new_token = make_token(line, t_state->trim_start,
+			t_state->current_pos + i - t_state->trim_start,
+			t_state->token_kind);
 	if (!new_token)
 		return (FAIL);
 	if (ft_lstadd_token(token_list, new_token) == FAIL)
 		return (FAIL);
-	tokenize_state->current_pos += i;
-	tokenize_state->trim_start = tokenize_state->current_pos;
+	t_state->current_pos += i;
+	t_state->trim_start = t_state->current_pos;
 	return (SUCCESS);
 }
 
 int	separate_by_null_char(char *line, t_list **token_list,
-	t_tokenize_state *tokenize_state)
+	t_tokenize_state *t_state)
 {
 	size_t	i;
 	t_token	*new_token;
 
 	i = 0;
-	tokenize_state->current_pos++;
-	new_token = make_token(line, tokenize_state->trim_start,
-			tokenize_state->current_pos - tokenize_state->trim_start, TK_WORD);
+	t_state->current_pos++;
+	new_token = make_token(line, t_state->trim_start,
+			t_state->current_pos - t_state->trim_start, TK_WORD);
 	if (!new_token)
 		return (FAIL);
 	if (ft_lstadd_token(token_list, new_token) == FAIL)
 		return (FAIL);
-	tokenize_state->trim_start = tokenize_state->current_pos + i;
+	t_state->trim_start = t_state->current_pos + i;
 	return (SUCCESS);
 }
--- srcs/tokenize/tokenize.c
+++ srcs/tokenize/tokenize.c
@@ -11,43 +11,43 @@ void	update_quote_state(t_quote_state *state, char c)
 		*state = NORMAL;
 }
 
-int	read_line(char *line, t_list **token_list, t_tokenize_state *tokenize_state)
+int	read_line(char *line, t_list **token_list, t_tokenize_state *t_state)
 {
-	if (is_metacharacter_without_token_kind(line[tokenize_state->current_pos])
-		&& tokenize_state->quote_state == NORMAL)
-		return (separate_by_no_kind_sep_word(line, token_list, tokenize_state));
-	else if ((is_separating_word(line, tokenize_state->current_pos)
-			&& tokenize_state->quote_state == NORMAL))
-		return (separate_by_sep_word(line, token_list, tokenize_state));
-	else if (line[tokenize_state->current_pos + 1] == '\0')
-		return (separate_by_null_char(line, token_list, tokenize_state));
+	if (is_metacharacter_without_token_kind(line[t_state->current_pos])
+		&& t_state->quote_state == NORMAL)
+		return (separate_by_no_kind_sep_word(line, token_list, t_state));
+	else if ((is_separating_word(line, t_state->current_pos)
+			&& t_state->quote_state == NORMAL))
+		return (separate_by_sep_word(line, token_list, t_state));
+	else if (line[t_state->current_pos + 1] == '\0')
+		return (separate_by_null_char(line, token_list, t_state));
 	else
-		tokenize_state->current_pos++;
+		t_state->current_pos++;
 	return (SUCCESS);
 }
 
-void	init_tokenize_state(t_tokenize_state *tokenize_state)
+void	init_tokenize_state(t_tokenize_state *t_state)
 {
-	tokenize_state->trim_start = 0;
-	tokenize_state->current_pos = 0;
-	tokenize_state->quote_state = NORMAL;
-	tokenize_state->token_kind = TK_WORD;
+	t_state->trim_start = 0;
+	t_state->current_pos = 0;
+	t_state->quote_state = NORMAL;
+	t_state->token_kind = TK_WORD;
 }
 
 int	tokenize(char *line, t_list **token_list)
 {
-	t_tokenize_state	tokenize_state;
+	t_tokenize_state	t_state;
 
-	init_tokenize_state(&tokenize_state);
-	while (line[tokenize_state.current_pos]
-		&& tokenize_state.current_pos < ft_strlen(line))
+	init_tokenize_state(&t_state);
+	while (line[t_state.current_pos]
+		&& t_state.current_pos < ft_strlen(line))
 	{
-		update_quote_state(&tokenize_state.quote_state,
-			line[tokenize_state.current_pos]);
-		if (read_line(line, token_list, &tokenize_state) == FAIL)
+		update_quote_state(&t_state.quote_state,
+			line[t_state.current_pos]);
+		if (read_line(line, token_list, &t_state) == FAIL)
 			return (FAIL);
 	}
-	if (tokenize_state.quote_state != NORMAL)
+	if (t_state.quote_state != NORMAL)
 	{
 		printf("minishell: syntax error: unexpected end of file\n");
 		return (FAIL);
