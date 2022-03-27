fca83d8b03fffbc4ac710352ff5939b622571c93
Hiroaki-K4 <49no19@gmail.com>
2022-03-27 21:30:02 +0900

    #124 Delete commentout

--- srcs/expand/expand_word_in_heredocument.c
+++ srcs/expand/expand_word_in_heredocument.c
@@ -6,33 +6,11 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 17:00:30 by hkubo             #+#    #+#             */
-/*   Updated: 2022/03/27 21:20:40 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/03/27 21:28:47 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// char	*get_env_name_here_ver(char *input, size_t *current_pos)
-// {
-// 	size_t	start;
-
-// 	start = *current_pos;
-// 	while (input[*current_pos]
-// 		&& input[*current_pos] != ' '
-// 		&& input[*current_pos] != '$'
-// 		&& input[*current_pos] != '\''
-// 		&& input[*current_pos] != '\"'
-// 		&& input[*current_pos] != ':'
-// 	)
-// 	{
-// 		(*current_pos)++;
-// 		if (input[*current_pos - 1] == '?')
-// 			return (ft_strdup("$?"));
-// 	}
-// 	if (*current_pos == start)
-// 		return (ft_strdup("$"));
-// 	return (ft_substr(input, start, *current_pos - start));
-// }
 
 char	*check_name_here_ver(char *name, t_envs *envs, int exit_status)
 {
